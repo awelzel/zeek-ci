c0bf1cc160d65ddef84539b3fb42c7e926d5770a
Arne Welzel <arne.welzel@corelight.com>
2023-01-24 11:01:49 +0100

    Initial import from zeek/zeek

---
+++ freebsd/prepare.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Preparing FreeBSD environment"
+sysctl hw.model hw.machine hw.ncpu
+set -e
+set -x
+
+env ASSUME_ALWAYS_YES=YES pkg bootstrap
+pkg install -y bash git cmake swig bison python3 base64 flex ccache
+pkg upgrade -y curl
+pyver=$(python3 -c 'import sys; print(f"py{sys.version_info[0]}{sys.version_info[1]}")')
+pkg install -y $pyver-sqlite3 $pyver-pip
+
+python -m pip install websockets junit2html
+
+# Spicy detects whether it is run from build directory via `/proc`.
+echo "proc /proc procfs rw,noauto 0 0" >>/etc/fstab
+mount /proc
